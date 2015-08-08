3f11990687adc3c9a83ca14bbafa20524fd8cefd
andrewszw <zacharyandrews93@gmail.com>
2015-08-08 12:48:32 -0400

    randomly adds teams to a text file

--- createTextFile.py
+++ createTextFile.py
@@ -1,15 +1,27 @@
+import random
+
 def main():
-    teams = getTeamNames()
+    numTeams = int(input("Please enter the number of teams you wish to add to the tournament: "))
+    numGroups = int(input("Please enter the number of groups you wish to make with the teams: "))
+    teams = getTeamNames(numTeams)
+    randomTeams = random.sample(teams, numTeams)
+    toTextFile(randomTeams)
 
 """ Get the team names and store then in a list """
-def getTeamNames():
-    numTeams = int(input("Please enter the number of teams you wish to add to the tournament: "))
+def getTeamNames(numTeams):
     teamList = []
     count = 1
     while count <= numTeams:
-        teamName = input("Please enter the name of team number %d " % count)
+        teamName = raw_input("Please enter the name of team number %d: " % count)
         teamList.append(teamName)
         count += 1
     return teamList
+
+""" Randomly place teams into a text file """
+def toTextFile(randomTeams):
+    myFile = open("teams.txt", "w")
+    for team in randomTeams:
+        myFile.write(team + "\n")
+    myFile.close()
 
 main()
